c4e7a710573987c63fee733fe4ebee40d66f14e2
Leonardo Gonçalves Pinezi Domingues <glitchpalm@glitch.bbrouter>
2025-03-02 16:58:55 -0300

    Deploy

--- pages/index.js
+++ pages/index.js
@@ -1,27 +1,14 @@
-import { useState } from "react";
+import Link from "next/link"
 
 function Home() {
     return (
         <div>
             <h1>Home</h1>
-            <Contador />
+            <Link href="/sobre">
+                <a>Sobre</a>
+            </Link>
         </div>
     )
-}
-
-function Contador() {
-    const [contador, setContador] = useState(0);
-
-    function adicionarContador() {
-        setContador(contador+1);
-    }
-
-    return (
-        <div>
-            <div>{contador}</div>
-            <button onClick={adicionarContador}>Adicionar</button>
-        </div>
-    );
 }
 
 export default Home;
